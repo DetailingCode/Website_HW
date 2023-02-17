d917631f6a235b653f8f9bad4ed4a964bafd038a
DetailingCode <doniek77@o2.pl>
2023-02-17 22:54:53 +0100

    section about us added, 2 div, left text and right img

---
+++ js/slicksettings.js
@@ -0,0 +1,24 @@
+$(".team-carousel").slick({
+	mobileFirst: true,
+	autoplay: true,
+	autoplaySpeed: 3000,
+	arrows: false,
+	infinite: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	responsive: [
+		{
+			breakpoint: 576,
+			settings: { slidesToShow: 2 },
+		},
+        {
+			breakpoint: 768,
+			settings: { slidesToShow: 2 },
+		},
+
+		{
+			breakpoint: 992,
+			settings: { slidesToShow: 3 },
+		},
+	],
+});
