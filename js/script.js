09f9afb011fe5c83f48a1ef380ac7934a3916d15
DetailingCode <doniek77@o2.pl>
2023-03-01 08:20:51 +0100

    changed for work

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const navList = document.querySelector('.navbar-collapse')
 
     function addShadow() {
-        if (window.scrollY >= 10) {
+        if (window.scrollY >= 1) {
             nav.classList.add('shadow-bg')
         } else {
             nav.classList.remove('shadow-bg')
