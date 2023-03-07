b4b61a258a430d516eae645efb9eeebd1dfeb2f3
DetailingCode <doniek77@o2.pl>
2023-03-07 15:21:31 +0100

    accessory section finished

--- js/slicksettings.js
+++ js/slicksettings.js
@@ -1,4 +1,4 @@
-$(".").slick({
+$(". ").slick({
 	mobileFirst: true,
 	autoplay: true,
 	autoplaySpeed: 6000,
@@ -22,3 +22,30 @@ $(".").slick({
 		},
 	],
 });
+
+
+$('.center').slick({
+	centerMode: true,
+	centerPadding: '60px',
+	slidesToShow: 3,
+	responsive: [
+	  {
+		breakpoint: 768,
+		settings: {
+		  arrows: false,
+		  centerMode: true,
+		  centerPadding: '40px',
+		  slidesToShow: 3
+		}
+	  },
+	  {
+		breakpoint: 480,
+		settings: {
+		  arrows: false,
+		  centerMode: true,
+		  centerPadding: '40px',
+		  slidesToShow: 1
+		}
+	  }
+	]
+  });
